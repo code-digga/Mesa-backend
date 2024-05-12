16c071211642d5980cfd55b0e42c040fe45ca3c2
code-digga <168196193+code-digga@users.noreply.github.com>
2024-05-12 21:54:59 +0100

    Improved the logic in the controller

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,11 +1,13 @@
 const { User } = require("../models/models_associations");
+const { generateToken } = require("../utils/crypto_jwt");
 
 const signUp = async (req, res, next) => {
   // making sure no body param is empty
-  for (const [key, val] in Object.entries(req.body)) {
+  for (const [key, val] of Object.entries(req.body)) {
     if (!val.trim()) {
       const error = new Error(`${key} value is not valid.`);
       next(error);
+      return;
     }
   }
   const { full_name, email, user_password, user_type } = req.body;
@@ -20,10 +22,12 @@ const signUp = async (req, res, next) => {
 
     const { password, ...others } = newUser;
 
+    const userToken = await generateToken(email, user_password);
+
     res.status(201).json({
       success: true,
       message: "User created successfully",
-      data: others,
+      data: { token: userToken, ...others },
     });
   } catch (error) {
     next(err);
