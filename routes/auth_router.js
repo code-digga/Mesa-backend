acca1efc8156bd2a028203a5c3a3b4deca7eb9be
code-digga <168196193+code-digga@users.noreply.github.com>
2024-05-14 21:21:23 +0100

    login middleware in route

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -2,6 +2,6 @@ const authController = require("../controllers/auth_controller");
 const router = require("express").Router();
 
 router.post("/register", authController.signUp);
-router.post("/login");
+router.post("/login", authController.signIn);
 
 module.exports = router;
