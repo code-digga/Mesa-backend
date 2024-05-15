6a47704c31c35183df68773c5098b9fdc9b0ed0e
code-digga <168196193+code-digga@users.noreply.github.com>
2024-05-15 21:44:58 +0100

    fis: import file error

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,5 +1,5 @@
 const authController = require("../controllers/auth_controller");
-const middleware = require("../middlewares/auth_middlewares/auth_validation");
+const middleware = require("../middlewares/validators/auth_validation");
 const router = require("express").Router();
 
 router.post("/register", middleware.validateRegister, authController.signUp);
