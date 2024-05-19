2ed5e294c05ae6db9f96968e6b74ec935e5f3643
code-digga <168196193+code-digga@users.noreply.github.com>
2024-05-19 22:11:52 +0100

    completed initial results route and controller

--- routes/results_route.js
+++ routes/results_route.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
 const authMiddlewares = require("../middlewares/authentication/auth_middleware");
+const middlewares = require("../middlewares/validators/results_validation");
+const controller = require("../controllers/results_controller");
 
-router.get("/", authMiddlewares.checkToken);
-router.post("/", authMiddlewares.verifyStudent);
-router.put("/", authMiddlewares.verifyStudent);
+router.get(
+  "/",
+  authMiddlewares.verifyTeacher,
+  middlewares.validateResultFetch,
+  controller.fetchAllResults
+);
+router.get(
+  "/:id",
+  authMiddlewares.verifyStudent,
+  middlewares.validateResultFetch,
+  controller.fetchUserResults
+);
+router.post(
+  "/",
+  authMiddlewares.verifyStudent,
+  middlewares.validateCreatingRecord,
+  controller.createRecord
+);
+router.put(
+  "/",
+  authMiddlewares.verifyStudent,
+  middlewares.validateUpdatingRecord,
+  controller.updateRecord
+);
 
 module.exports = router;
